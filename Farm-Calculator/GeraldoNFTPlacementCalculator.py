1a1f8d18792e1ca785b05eb2ecf2ccf8951a25f7
pi61 <83331555+pi61@users.noreply.github.com>
2025-01-01 11:36:48 -0700

    Update GeraldoNFTPlacementCalculator.py

--- Farm-Calculator/GeraldoNFTPlacementCalculator.py
+++ Farm-Calculator/GeraldoNFTPlacementCalculator.py
@@ -1,7 +1,7 @@
-#Determines the optimal round to purchase Geraldo NFT for 2-player co-op mode
+#Determines the optimal round to purchase Geraldo NFT
 import math
 
-STARTING_CASH = 1450
+STARTING_CASH = 650
 DIFFICULTY = 1
 MONKEY_KNOWLEDGE = True
 
@@ -10,6 +10,7 @@ baseNFTCost = [550,650,700][DIFFICULTY]
 
 if MONKEY_KNOWLEDGE:
     baseGerryCost *= 0.9
+    STARTING_CASH += 200
 
 budget = [121, 137, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266,     268, 165, 358, 260]
 budget = [v-1 for v in budget]
@@ -26,48 +27,41 @@ def Round31Value(gerryRound):
     sellValue = 550 + (cost - 550) * 0.95
     return sellValue
     
-def check(i, j):
+def checkValue(gerryRounds):
     cash = 0
-    placedFirstGerry = -1
-    placedFirstNFT = -1
-    placedSecondGerry = -1
-    placedSecondNFT = -1
+    gerryRounds.sort()
+    gerryPlaced = [False for v in gerryRounds]
+    nftRounds = [-1 for v in gerryRounds]
     cashSpentOnNFT = 0
+    totalValue = 0
+    
     for roundNumber in range(0, 18):
         cash += budget[roundNumber]
         
-        if roundNumber == i:
-            if cash < baseGerryCost: 
-                return -1
-            else:
-                placedFirstGerry = roundNumber
-                cash -= baseGerryCost
+        for gerry in range(0, len(gerryRounds)):
+            if (gerryPlaced[gerry] == False):
+                if (gerryRounds[gerry] != roundNumber):
+                    break
+                elif (gerry > 0 and nftRounds[gerry - 1] == -1) or cash < baseGerryCost:
+                    return -1
+                else:
+                    gerryPlaced[gerry] = True
+                    cash -= baseGerryCost
+                    
+            if gerryPlaced[gerry] == True and nftRounds[gerry] == -1:
+                cost = nftCost(gerryRounds[gerry], roundNumber)
+                if cash >= cost:
+                    cash -= cost 
+                    cash += round(baseGerryCost * 0.75) #Sells Geraldo
+                    nftRounds[gerry] = roundNumber
+                    cashSpentOnNFT += cost
         
-        if placedFirstGerry > -1 and placedFirstNFT == -1:
-            if cash >= nftCost(placedFirstGerry, roundNumber):
-                cash -= nftCost(placedFirstGerry, roundNumber) - round(baseGerryCost * 0.75)
-                placedFirstNFT = roundNumber
-                cashSpentOnNFT += nftCost(placedFirstGerry, roundNumber)
-                
-        if roundNumber == j:
-            if placedFirstNFT == -1:
-                return -1
-            elif cash < baseGerryCost:
-                return -1
-            else:
-                placedSecondGerry = roundNumber
-                cash -= baseGerryCost
-                
-        if placedSecondGerry > -1 and placedSecondNFT == -1:
-            if cash >= nftCost(placedSecondGerry, roundNumber):
-                cash -= nftCost(placedSecondGerry, roundNumber)
-                placedSecondNFT = roundNumber
-                cashSpentOnNFT += nftCost(placedSecondGerry, roundNumber)
-    
-    if (placedSecondNFT == -1):
-        return -1
+    for gerry in range(0, len(gerryRounds)):
+        if nftRounds[gerry] == -1:
+            return -1
+        totalValue += Round31Value(gerryRounds[gerry])
         
-    return [cashSpentOnNFT, Round31Value(placedFirstGerry) + Round31Value(placedSecondGerry), placedFirstNFT + 1, placedSecondNFT + 1]
+    return [totalValue, cashSpentOnNFT, gerryRounds, nftRounds]
     
     
 def main():
@@ -77,19 +71,19 @@ def main():
     cashSpent = 0
     for i in range(0, 10):
         for j in range(i, 10):
-            result = check(i,j)
+            result = checkValue([i,j])
             if result == -1:
                 continue
             
             print([i + 1, j + 1, result])
             
-            if maxResult < result[1]:
-                maxResult = result[1]
-                cashSpent = result[0]
+            if maxResult < result[0]:
+                maxResult = result[0]
+                cashSpent = result[1]
                 firstRound = i + 1
                 secondRound = j + 1
             
-            break   
+            break
     
     print(str(maxResult) + " is the maximum value for " + str(cashSpent) + " cash spent on NFTs.")
     print("Placement round of the 2 Geraldos are " + str(firstRound) + " and " + str(secondRound))
