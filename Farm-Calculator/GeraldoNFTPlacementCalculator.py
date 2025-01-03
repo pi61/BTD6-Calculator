464a0fc911c5e0ae7a1481e9afd88b0c3890593d
pi61 <83331555+pi61@users.noreply.github.com>
2025-01-03 03:14:09 -0700

    Update GeraldoNFTPlacementCalculator.py
    - Added custom round types
    - Support for adding defense cost

--- Farm-Calculator/GeraldoNFTPlacementCalculator.py
+++ Farm-Calculator/GeraldoNFTPlacementCalculator.py
@@ -4,16 +4,32 @@ The 10 results with the best (value - cash spent) are printed.
 Assumes all MK are enabled.
 '''
 import math
+from enum import Enum
 
+class RoundType(Enum):
+    REGULAR = [0, 121, 137, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 167, 335, 333, 662, 266, 389, 337]
+    VORTEX = [0, 121, 137, 138, 175, 188, 163, 182, 200, 199, 314, 189, 222, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 228, 335, 333, 662, 266, 389, 378]
+    ALTERNATE = [0, 121, 157, 140, 180, 157, 165, 197, 193, 199, 320, 220, 185, 285, 258, 331, 281, 177, 448, 251, 186, 179, 298, 358, 147, 345, 454, 662, 266, 467, 337]
+    CUSTOM = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #Always leave a starting 0, for "Round 0"
+    
 ############## USER INPUT ##################
 STARTING_ROUND = 1
 STARTING_CASH = 850 #If you have extra starting cash MK enabled, take it into account.
 DIFFICULTY = 1 # 0 - easy, 1 - medium, 2 - hard, 3 - impoppable
-NUMBER_OF_PLAYERS = 4
+NUMBER_OF_PLAYERS = 1
 NUMBER_OF_RESULTS = 10
 PRETTY = False #False for a compact output. True for an extended, readable output
-############################################
+ROUND_TYPE = RoundType.VORTEX 
 
+#Cash spent on defense on certain rounds, after getting some income from round. Format: [Round, Cash spent, Duration (optional)]
+DEFENSE_INPUT = [
+    [5, 150], # $150 spent at start of r5
+    [10, 200, 15], # $200 spent on r10 after collecting $15 from round income
+    [10, 250, 7],
+    [7, 300]
+]
+    
+########### INITIALIZE VARIABLES ###########
 def floor5(number):
     return math.floor(number / 5) * 5
     
@@ -24,12 +40,53 @@ difficultyMultiplier = [0.85, 1, 1.08, 1.2]
 baseGerryCost = roundTo5(750 * 0.9 * difficultyMultiplier[DIFFICULTY])
 baseNFTCost = 650 * difficultyMultiplier[DIFFICULTY]
 
-roundIncome = [0, 121, 137, 138, 175, 164, 163, 182, 200, 199, 314, 189, 192, 282, 259, 266, 268, 165, 358, 260, 186, 351, 298, 277, 167, 335, 333, 662, 266, 389, 337]
-roundBudget = [v-1 for v in roundIncome]
-roundBudget[STARTING_ROUND] += STARTING_CASH - 100 - STARTING_ROUND
+roundIncome = ROUND_TYPE.value
+roundFarmingBudget = [v - 1 for v in roundIncome]
+roundFarmingBudget[STARTING_ROUND - 1] = STARTING_CASH - 100 - STARTING_ROUND
+roundDefensiveBudget = [0 for v in roundFarmingBudget]
 
-resultList = []
+for i in DEFENSE_INPUT:
+    if (len(i) == 2):
+        i.append(0)
+
+DEFENSE_INPUT.sort(key=lambda arr: (arr[0],arr[2], arr[1]))
+defenseRounds = [[] for i in range(0,31)]
+for defense in DEFENSE_INPUT:
+    roundNumber = defense[0]
+    if (roundFarmingBudget[roundNumber] - (100 + roundNumber - 1) < defense[2]):
+        print("Duration value for " + str(defense) + " is too large.")
+        print("Round " + str(defense[0]) + " income duration is " + str(roundFarmingBudget[roundNumber] - (100 + roundNumber - 1)))
+        exit(0)
     
+    defenseRounds[defense[0]].append(defense)
+    
+
+#Modify farming budget to account for defensive budget
+debt = 0
+for i in range(30, STARTING_ROUND - 1, -1):
+    defenses = defenseRounds[i]
+    startingCash = 0
+    sumOfSpending = 0
+    
+    for defense in defenses:
+        sumOfSpending += defense[1]
+        startingCash = max(0, sumOfSpending - defense[2])
+    
+    debt += max(0, startingCash - (100 + i - 1))
+    temp = min(roundFarmingBudget[i - 1], debt)
+    roundDefensiveBudget[i - 1] += temp
+    roundFarmingBudget[i - 1] -= temp
+    debt -= temp
+    roundFarmingBudget[i] -= (sumOfSpending - max(0, startingCash - (100 + i - 1)))  
+    roundDefensiveBudget[i] += (sumOfSpending - max(0, startingCash - (100 + i - 1)))
+    
+if (debt > 0):
+    print("Defensive budget too large. Unable to saveup.")
+    exit(0)
+            
+resultList = [] 
+
+############# FUNCTIONS ####################
 def nftCost(gerryRound, nftRound):
     power = max(0, nftRound - gerryRound - 1)
     multiplier = 1.1 ** power
@@ -42,35 +99,37 @@ def nftSellValue(gerryRound, sellRound):
     return sellValue
     
 def checkValue(gerryRounds):
-    cash = 0
+    farmingBudget = 0
+    defensiveBudget = 0
     gerryRounds.sort()
     gerryPlaced = [[-1, 0] for v in gerryRounds]
     nftRounds = [[-1, 0] for v in gerryRounds]
     cashSpentOnNft = 0
     totalValue = 0
     
-    for roundNumber in range(STARTING_ROUND, 31):
-        cash += roundBudget[roundNumber]
+    for roundNumber in range(STARTING_ROUND - 1, 31):
+        farmingBudget += roundFarmingBudget[roundNumber]
+        defensiveBudget += roundDefensiveBudget[roundNumber]
         
         for gerry in range(0, len(gerryRounds)):
             if (gerryPlaced[gerry][0] == -1):
                 if (gerryRounds[gerry] != roundNumber):
                     break
-                elif (gerry > 0 and nftRounds[gerry - 1][0] == -1) or cash < baseGerryCost:
+                elif (gerry > 0 and nftRounds[gerry - 1][0] == -1) or farmingBudget < baseGerryCost:
                     return 
                 else:
                     gerryPlaced[gerry][0] = roundNumber
-                    cash -= baseGerryCost
-                    gerryPlaced[gerry][1] = cash
+                    farmingBudget -= baseGerryCost
+                    gerryPlaced[gerry][1] = farmingBudget + defensiveBudget
                     
             if gerryPlaced[gerry][0] != -1 and nftRounds[gerry][0] == -1:
                 cost = nftCost(gerryRounds[gerry], roundNumber)
-                if cash >= cost:
-                    cash -= cost 
-                    nftRounds[gerry][1] = cash
+                if farmingBudget >= cost:
+                    farmingBudget -= cost 
+                    nftRounds[gerry][1] = farmingBudget + defensiveBudget
                     nftRounds[gerry][0] = roundNumber
                     cashSpentOnNft += cost
-                    cash += round(baseGerryCost * 0.75) #Sells previous Geraldo
+                    farmingBudget += round(baseGerryCost * 0.75) #Sells previous Geraldo
             
     for gerry in range(0, len(gerryRounds)):
         if nftRounds[gerry][0] == -1:
@@ -106,7 +165,8 @@ def compactPrint():
     print()
     for i in range(0, min(NUMBER_OF_RESULTS, len(resultList))):
         print(resultList[i])
-            
+        
+############### DRIVER FUNCTION ############
 def main():
     for i in range(STARTING_ROUND, 31):
         if NUMBER_OF_PLAYERS == 1:
@@ -130,4 +190,5 @@ def main():
         prettyPrint()
     else:
         compactPrint()
+        
 main()
