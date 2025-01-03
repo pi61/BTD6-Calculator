6e2aacad88cd1e78a0e7111efc42eb460f851797
pi61 <83331555+pi61@users.noreply.github.com>
2025-01-03 15:09:10 -0700

    Update GeraldoNFTPlacementCalculator.py

--- Farm-Calculator/GeraldoNFTPlacementCalculator.py
+++ Farm-Calculator/GeraldoNFTPlacementCalculator.py
@@ -157,7 +157,6 @@ def prettyPrint():
         print("--------------- Result #" + str(i) + "------------------")
         result = resultList[i]
         print("Max profit at r31 is $" + str(result[0]) + " for $" + str(result[1]) + " spent on NFTs.")
-        print("Geraldo placement rounds:")
         
         for i in range(0, NUMBER_OF_PLAYERS):
             print("#" + str(i + 1) + " Geraldo placement round: " + str(result[2][i][0]))
